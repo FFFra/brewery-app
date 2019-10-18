2f86b901c6ed5cb9a4da173d97164c1d2b6c7b95
Flávio Fraschetti <flavio.fraschetti@indigoway.com>
2019-10-18 16:41:58 +0100

    feat(structure): generates Error page & Loading + Header components

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            <p>Hellow from loading</p>
+        </div>
+    );
+};
+
+export default loading;
---
+++ src/pages/Error/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <p>Hello from Error</p>
+        </div>
+    );
+};
+
+export default Error;
