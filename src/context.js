4922f3bde8b0362288dad1a0125214581c9f51e0
Flávio Fraschetti <flavio.fraschetti@indigoway.com>
2019-10-18 14:43:34 +0100

    improvement(base): helper functions to format data

---
+++ src/context.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import {loadBeers} from './services/api';
+
+const BeerContext = React.createContext();
+
+class BeerProvider extends Component {
+    state = {
+        bottles: [],
+        loading: true
+    };
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData = () =>
+        loadBeers().then(res => {
+            let data = res.data;
+            let bottles = this.formatData(data);
+            this.setState({bottles, loading: false});
+            return bottles;
+        });
+
+    getBottleDetail = slug => {
+        let tempBottle = [...this.state.bottles];
+        const bottle = tempBottle.find(bottle => bottle.slug === slug);
+        return bottle;
+    };
+
+    formatData(data) {
+        let tempData = data.map(item => {
+            let tempStyle = item.style;
+            let beerStyle = tempStyle ? tempStyle.name : 'Unknown style';
+            let description = tempStyle
+                ? tempStyle.description
+                : 'no description';
+            let slug = this.generateSlug(item.name);
+            let ibu = item.ibu ? item.ibu : 'N/D';
+
+            let bottles = {
+                ...item,
+                beerStyle,
+                description,
+                slug,
+                ibu
+            };
+            return bottles;
+        });
+        return tempData;
+    }
+
+    generateSlug(name) {
+        return name
+            .split(' ')
+            .join('-')
+            .toLowerCase();
+    }
+
+    render() {
+        return (
+            <BeerContext.Provider
+                value={{...this.state, getBottleDetail: this.getBottleDetail}}>
+                {this.props.children}
+            </BeerContext.Provider>
+        );
+    }
+}
+
+const BeerConsumer = BeerContext.Consumer;
+
+export {BeerProvider, BeerConsumer, BeerContext};
