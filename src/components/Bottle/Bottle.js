10b2bdfe26e022c0a3e29fb308220e1f6e6c3b2c
Flávio Fraschetti <flavio.fraschetti@indigoway.com>
2019-10-20 02:17:35 +0100

    style(component): add card styles and animation to each item

--- src/components/Bottle/Bottle.js
+++ src/components/Bottle/Bottle.js
@@ -2,21 +2,24 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import './Bottle.css';
+
 const Bottle = ({item}) => {
     const {name, beerStyle, ibu, abv, status, slug} = item;
 
     return (
-        <div>
+        <article className='bottle__card'>
             <h1>{name}</h1>
             <h4>{status}</h4>
             <p>style: {beerStyle}</p>
             <span>ibu: {ibu} </span>
             <span>abv: {abv} </span>
-
-            <Link to={`/details/${slug}`}>
-                <button>Show details</button>
-            </Link>
-        </div>
+            <div>
+                <Link to={`/details/${slug}`}>
+                    <button>Show details</button>
+                </Link>
+            </div>
+        </article>
     );
 };
 
