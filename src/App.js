9cd53131a09278c05a41821a69b4f5da2fe6da25
Flávio Fraschetti <flavio.fraschetti@indigoway.com>
2019-10-20 02:24:24 +0100

    refactor(component): render header on all pages

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import {Route, Switch} from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Error from './pages/Error/Error';
 import BeerDetails from './pages/BeerDetails/BeerDetails';
+import Header from './components/Header/Header';
 
 function App() {
     return (
         <>
+            <Header title='welcome to' />
             <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/details/:slug' component={BeerDetails} />
